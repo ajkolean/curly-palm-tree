ef485d235151cc77b6fb8a481fee228a15460335
Andy Kolean <akolean@fundrise.com>
2021-11-07 07:33:04 -0500

    add initial home view UI

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -26,12 +26,7 @@ struct OnboardingView: View {
                         .padding(.horizontal, 10)
                 }
                 ZStack {
-                    Circle()
-                        .stroke(.white.opacity(0.2), lineWidth: 40)
-                        .frame(width: 260, height: 260, alignment: .center)
-                    Circle()
-                        .stroke(.white.opacity(0.2), lineWidth: 80)
-                        .frame(width: 260, height: 260, alignment: .center)
+                    CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
@@ -88,3 +83,5 @@ struct OnboardingView_Previews: PreviewProvider {
         OnboardingView()
     }
 }
+
+
