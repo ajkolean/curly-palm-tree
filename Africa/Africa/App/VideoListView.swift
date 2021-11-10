ed5edb94015af4ef49194d568e80c32ab5dcee3c
Andy Kolean <akolean@fundrise.com>
2021-11-10 08:09:32 -0500

    add Africa app, add resources data, add project structure

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct VideoListView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+// MARK: - PREVIEW
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
