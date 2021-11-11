89b48ddb80788139ffd983c8b5d81dadd704c18a
Andy Kolean <akolean@fundrise.com>
2021-11-11 04:37:37 -0500

    finish animal detail view

---
+++ Africa/Africa/View/ExternalWeblinkView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ExternalWeblinkView: View {
+    // MARK: - PROPERTIES
+    let animal: Animal
+
+    // MARK: - BODY
+    var body: some View {
+        GroupBox {
+            HStack {
+                Image(systemName: "globe")
+                Text("Wikipedia")
+                Spacer()
+
+                Group {
+                    Link(animal.name, destination: URL(string: animal.link) ?? URL(string: "https://wikipedia.org")!)
+                    Image(systemName: "arrow.up.right.square")
+                }
+                .foregroundColor(.accentColor)
+            } //: HSTACK
+        } //: BOX
+    }
+}
+
+// MARK: - PREVIEW
+struct ExternalWeblinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalWeblinkView(animal: Bundle.animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
---
+++ Africa/Africa/View/InsetFactView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct InsetFactView: View {
+    // MARK: - PROPERTIES
+    let animal: Animal
+
+    // MARK: - BODY
+    var body: some View {
+        GroupBox {
+            TabView {
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                }
+            } //: TABS
+            .tabViewStyle(.page)
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        } // : BOX
+    }
+}
+
+// MARK: - PREVIEW
+struct InsetFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetFactView(animal: Bundle.animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
