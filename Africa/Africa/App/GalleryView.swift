cc02283659ebd7ac6a7d36061cb798d11c2f433c
Andy Kolean <akolean@fundrise.com>
2021-11-11 08:30:35 -0500

    add grid layout

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -3,13 +3,67 @@ import SwiftUI
 struct GalleryView: View {
     // MARK: - PROPERTIES
 
+    @State private var selectedAnimal = "lion"
+    let animals = Bundle.animals
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+
+//    let gridLayout = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+
+    @State private var gridLayout = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.white, lineWidth: 8))
+
+                // MARK: - SLIDER
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { _ in
+                        withAnimation(.easeOut) {
+                            gridSwitch()
+                        }
+                    }
+
+                // MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                } //: GRID
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
+    }
+
+    // MARK: - Helpers
+    private func gridSwitch() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
     }
 }
 
