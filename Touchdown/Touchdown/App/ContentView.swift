79200382d67a309f2d5ede6ffcbf518d581f954e
Andy Kolean <akolean@fundrise.com>
2021-11-22 09:08:41 -0500

    add navigation bar view

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -5,8 +5,17 @@ struct ContentView: View {
 
     // MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        VStack(spacing: 0) {
+            NavigationBarView()
+                .padding()
+                .background(Color.white)
+                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+
+            Spacer()
+            FooterView()
+                .padding(.horizontal)
+        } //: VSTACK
+        .background(colorBackground.ignoresSafeArea(.all, edges: .all))
     }
 }
 
