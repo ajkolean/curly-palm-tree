eeb534c3edf69d51f96d5436cbd41d16236f9249
Andy Kolean <akolean@fundrise.com>
2021-11-22 08:27:10 -0500

    setup touch down project and add footer view

---
+++ Touchdown/Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Andy Kolean on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touchdown/Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct FooterView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and druable football helmets in the the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+
+            Text("Copyright © Andy Kolean\nAll rights reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
