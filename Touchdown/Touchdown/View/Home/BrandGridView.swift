b7bbf1f00e2a883ca1e419c5f1cfc10b22b0d9a2
Andy Kolean <akolean@fundrise.com>
2021-11-23 08:47:55 -0500

    add horizontal grid for brands

---
+++ Touchdown/Touchdown/View/Home/BrandGridView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct BrandGridView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: gridLayout, spacing: columnSpacing) {
+                ForEach(brands) { brand in
+                    BrandItemView(brand: brand)
+                }
+            } //: GRID
+            .frame(height: 200)
+            .padding(15)
+        } //: SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
