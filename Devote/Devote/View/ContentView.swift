ad72b3e4807492d45cca262b875a8ee39305ca79
Andy Kolean <akolean@fundrise.com>
2021-11-28 16:45:05 -0500

    setup core data model, add text input

--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -4,6 +4,10 @@ import CoreData
 struct ContentView: View {
     // MARK: - PROPERTIES
 
+    @State private var task: String = ""
+
+    private var isButtonDisabled: Bool { task.isEmpty }
+
     // MARK: - FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
 
@@ -16,27 +20,51 @@ struct ContentView: View {
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
+            VStack {
+                VStack(spacing: 16) {
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .cornerRadius(10)
+
+                    Button(action: {
+                        addItem()
+                    }, label: {
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
+                    }) //: BUTTON
+                        .disabled(isButtonDisabled)
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
+                        .cornerRadius(10)
+                } //: VSTACK
+                .padding()
+
+                List {
+                    ForEach(items) { item in
+                        VStack(alignment: .leading) {
+                            Text(item.task ?? "")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        } //: LIST ITEM
+                    } //: LOOP
+                    .onDelete(perform: deleteItems)
+                } //: LIST
+            } //: VSTACK
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
             } //: TOOLBAR
-            Text("Select an item")
         } //: NAVIGATION
     }
 
@@ -46,6 +74,9 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
+            newItem.task = task
+            newItem.isCompleted = false
+            newItem.id = UUID()
 
             do {
                 try viewContext.save()
@@ -53,6 +84,9 @@ struct ContentView: View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+
+            task = ""
+            hideKeyboard()
         }
     }
 
