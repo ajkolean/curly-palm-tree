d8bc7c518c048759982bb85db9a910654c78c83d
Andy Kolean <akolean@fundrise.com>
2021-11-28 21:05:29 -0500

    add notes watch app project setup and assets

---
+++ Notes/Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Andy Kolean on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
