d41ff2c31917cd738cb5c8b7f950f1378209e171
Andy Kolean <akolean@fundrise.com>
2021-12-02 05:04:15 -0500

    add initial avocados tab view

---
+++ Avocados/Avocados/View/AvocadosView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct AvocadosView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+    var body: some View {
+        Text("Avocados")
+    }
+}
+
+// MARK: - PREVIEW
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
---
+++ Avocados/Avocados/View/RipeningStagesView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct RipeningStagesView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+    var body: some View {
+        Text("Ripening Stages")
+    }
+}
+
+// MARK: - PREVIEW
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
+}
