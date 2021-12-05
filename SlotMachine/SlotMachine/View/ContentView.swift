f824cbd57bda10c47c6f2ca1bb6aa40a55796ad2
Andy Kolean <akolean@fundrise.com>
2021-12-05 04:18:41 -0500

    add game over modal

--- SlotMachine/SlotMachine/View/ContentView.swift
+++ SlotMachine/SlotMachine/View/ContentView.swift
@@ -17,6 +17,10 @@ struct ContentView: View {
     private var isActiveBet20: Bool {
         betAmount == 20
     }
+
+    private var isShowingModal: Bool {
+        coins <= 0
+    }
     
 
     // MARK: - BODY
@@ -176,8 +180,74 @@ struct ContentView: View {
             )
             .padding()
             .frame(maxWidth: 720)
+            .blur(radius: isShowingModal ? 5 : 0, opaque: false)
 
             // MARK: - POPUP
+            if isShowingModal {
+                ZStack {
+                    shadowColor.edgesIgnoringSafeArea(.all)
+
+                    // MODAL
+                    VStack(spacing: 0) {
+                        Text("GAME OVER")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(pinkColor)
+                            .foregroundColor(.white)
+
+                        Spacer()
+
+                        // MESAAGE
+                        VStack(alignment: .center, spacing: 16) {
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+
+                            Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                                .layoutPriority(1)
+
+                            Button(action: {
+                                coins = 100
+                            }, label: {
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .tint(pinkColor)
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundColor(pinkColor)
+                                    )
+                            }) //: BUTTON
+                        }
+
+                        Spacer()
+                    } //: VSTACK
+                    .frame(
+                        minWidth: 280,
+                        idealWidth: 280,
+                        maxWidth: 320,
+                        minHeight: 260,
+                        idealHeight: 280,
+                        maxHeight: 320,
+                        alignment: .center
+                    )
+                    .background(.white)
+                    .cornerRadius(20)
+                    .shadow(color: shadowColor, radius: 6, x: 0, y: 8)
+                } //: ZSTACK
+            }
+
         } //: ZSTACK
         .sheet(isPresented: $isShowingInfoView) {
             InfoView()
@@ -201,8 +271,6 @@ struct ContentView: View {
             }
         } else {
             playerLoses()
-
-            // GAME IS OVER
         }
     }
 
