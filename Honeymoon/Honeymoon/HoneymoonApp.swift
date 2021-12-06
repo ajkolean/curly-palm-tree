6a00fa5d95123094679d2f3cbe6fec01d7bd7a1f
Andy Kolean <akolean@fundrise.com>
2021-12-06 05:44:45 -0500

    setup honeymoon project and resources

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Andy Kolean on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
