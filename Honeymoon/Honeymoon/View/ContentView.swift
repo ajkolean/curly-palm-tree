9eb33726ba6e84c319a65f4725b7a71cd720a56c
Andy Kolean <akolean@fundrise.com>
2021-12-06 09:40:12 -0500

    add drag state enum

--- Honeymoon/Honeymoon/View/ContentView.swift
+++ Honeymoon/Honeymoon/View/ContentView.swift
@@ -6,11 +6,45 @@ struct ContentView: View {
     @State private var isShowingAlert = false
     @State private var isShowingGuideModal = false
     @State private var isShowingInfoModal = false
+    @GestureState private var dragState = DragState.inactive
 
     // MARK: - CARD VIEWS
 
-    var cardViews: [CardView] {
-        honeymoonData[0..<2].reversed().map { CardView(destination: $0) }
+    var cardViews: [CardView] = honeymoonData[0..<2].map { CardView(destination: $0) }
+
+    // MARK: - DRAG STATES
+
+    enum DragState: Equatable {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .inactive, .pressing:
+                return false
+            }
+        }
+
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
     }
 
     // MARK: - BODY
@@ -18,14 +52,29 @@ struct ContentView: View {
         VStack {
             // MARK: - HEADER
             HeaderView(isShowingGuideView: $isShowingGuideModal, isShowingInfoView: $isShowingInfoModal)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default, value: dragState)
 
             Spacer()
 
             // MARK: - CARDS
             ZStack {
-                ForEach(cardViews) { cardView in
+                ForEach(cardViews.reversed()) { cardView in
                     cardView
-//                        .zIndex(isTopCard(cardView: cardView) ? 1 : 0)
+                        .gesture(
+                            LongPressGesture(minimumDuration: 0.01)
+                                .sequenced(before: DragGesture())
+                                .updating($dragState) { value, state, transaction in
+                                    switch value {
+                                    case .first(true):
+                                        state = .pressing
+                                    case .second(true, let drag):
+                                        state = .dragging(translation: drag?.translation ?? .zero)
+                                    default:
+                                        break
+                                    }
+                                }
+                        )
                 }
             }
             .padding(.horizontal)
@@ -34,6 +83,8 @@ struct ContentView: View {
 
             // MARK: - FOOTER
             FooterView(showBookingAlert: $isShowingAlert)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default, value: dragState)
         } //: VSTACK
         .alert(isPresented: $isShowingAlert) {
             Alert(
@@ -45,11 +96,12 @@ struct ContentView: View {
     }
 
     // MARK: - HELPERS
+
     private func isTopCard(cardView: CardView) -> Bool {
-        guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
-            return false
-        }
-        return index == 0
+      guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
+        return false
+      }
+      return index == 0
     }
 }
 
