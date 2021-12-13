445c415521c0951f47b282dab8904f61c31e6407
Andy Kolean <akolean@fundrise.com>
2021-12-13 08:13:06 -0500

    add initial netflix clone app project

---
+++ NetflixClone/NetflixClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetflixClone
+//
+//  Created by Andy Kolean on 12/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetflixClone/NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Andy Kolean on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
